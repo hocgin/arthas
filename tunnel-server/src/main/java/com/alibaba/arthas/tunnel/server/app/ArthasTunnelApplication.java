985e472fcaa41b27a9ab51aa20c530cfd2f176de
hocgin <hocgin@gmail.com>
2021-09-20 17:03:24 +0800

    #

--- tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/app/ArthasTunnelApplication.java
+++ tunnel-server/src/main/java/com/alibaba/arthas/tunnel/server/app/ArthasTunnelApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = { "com.alibaba.arthas.tunnel.server.app",
         "com.alibaba.arthas.tunnel.server.endpoint" })
-//@EnableCaching
+@EnableCaching
 public class ArthasTunnelApplication {
 
     public static void main(String[] args) {
